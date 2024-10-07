98d4b8499e2ab6a4b67a2a2cbc2f2382881e85c5
mamingo19 <huytton119@gmail.com>
2024-10-07 23:13:39 +0700

    Done

--- 33_API/main.py
+++ 33_API/main.py
@@ -1,29 +1,51 @@
-from tkinter import *
 import requests
+from datetime import datetime
+import smtplib
+import time
 
+MY_EMAIL = "anonymous2528201122@gmail.com"
+PASSWORD = "yffpmpqogbpvxjer"
+MY_LAT = 10.957413
+MY_LONG = 106.842690
 
-def get_quote():
-    response = requests.get("https://api.kanye.rest")
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
     data = response.json()
-    quote = data["quote"]
-    canvas.itemconfig(quote_text, text=quote)
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL,
+                                msg=f"Subject:Look Up👆!\n\nThe ISS is above you in the sky bruh! LOOK.")
 
 
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
-
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
-canvas.grid(row=0, column=0)
-
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
 
 
-
-window.mainloop()
