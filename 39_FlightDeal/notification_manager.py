497007d5e4e9c52d1f8678fab0d69d46b5d8824d
mamingo19 <huytton119@gmail.com>
2024-10-22 16:40:35 +0700

    I dont know what am I doing

---
+++ 39_FlightDeal/notification_manager.py
@@ -0,0 +1,25 @@
+import os
+from twilio.rest import Client
+
+
+class NotificationManager:
+
+    def __init__(self):
+        self.client = Client(os.environ['TWILIO_SID'], os.environ["TWILIO_AUTH_TOKEN"])
+
+    def send_sms(self, message_body):
+        message = self.client.messages.create(
+            from_=os.environ["TWILIO_VIRTUAL_NUMBER"],
+            body=message_body,
+            to=os.environ["TWILIO_VIRTUAL_NUMBER"]
+        )
+        # Prints if successfully sent.
+        print(message.sid)
+
+    def send_whatsapp(self, message_body):
+        message = self.client.messages.create(
+            from_=f'whatsapp:{os.environ["TWILIO_WHATSAPP_NUMBER"]}',
+            body=message_body,
+            to=f'whatsapp:{os.environ["TWILIO_VERIFIED_NUMBER"]}'
+        )
+        print(message.sid)
