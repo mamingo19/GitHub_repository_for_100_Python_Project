c4467be78325b9ffc652afe223895dfe3a0ac3de
mamingo19 <huytton119@gmail.com>
2024-11-14 21:01:43 +0700

    finished task 1

---
+++ 46_Spotify/main.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+URL = "https://www.billboard.com/charts/hot-100/" + date
+response = requests.get(url=URL, headers=header)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
